58b527855c5bcb7a7358ad1e6392c931541ccbbf
MASDXI <sirawitt42@gmail.com>
2020-12-08 15:09:56 +0700

    update deployCC.sh

--- frist-network/deployCC.sh
+++ frist-network/deployCC.sh
@@ -8,9 +8,9 @@ export CC_SRC_PATH=../fabcar/go
 
 echo "====== Package Chaincode ======"
 
-export FABRIC_CFG_PATH=../config
+# export FABRIC_CFG_PATH=../config
 
-presetup() {
+pre-setup-chaincode() {
     echo Vendoring Go dependencies ...
     pushd $CC_SRC_PATH
     GO111MODULE=on go mod vendor
@@ -18,25 +18,47 @@ presetup() {
     echo Finished vendoring Go dependencies
 }
 
-# preset up chaincode 
-presetup
+# # preset up chaincode 
+pre-setup-chaincode
 
 echo "clean ${CC_NAME} binaries"
 sleep 2
 rm -rf $CC_NAME.tar.gz
 
 echo "generate ${CC_NAME} binaries"
-    
-../bin/peer lifecycle chaincode package ${CC_NAME}.tar.gz \
-    --path ${CC_SRC_PATH} \
-    --lang ${CC_RUNTIME_LANGUAGE} \
-    --label ${CC_NAME}_${CC_VERSION}
+
+# no sure right now using docker or export env 
+# ../bin/peer lifecycle chaincode package ${CC_NAME}.tar.gz \
+#     --path ${CC_SRC_PATH} \
+#     --lang ${CC_RUNTIME_LANGUAGE} \
+#     --label ${CC_NAME}_${CC_VERSION}
 
 # do something ...
 
 echo "====== install Chaincode ======"
 
-# do something ...
+install-chaincode-peer0-org2() {
+    echo "[peer0.org2.network.com] Installing..."
+
+    export PEER_CONTAINER_NAME=peer0.org1.network.com
+
+    docker exec ${PEER_CONTAINER_NAME} peer lifecycle chaincode install ${CC_NAME}.tar.gz
+
+    # do something ...
+}
+
+install-chaincode-peer0-org2() {
+    echo "[peer0.org2.network.com] Installing..."
+    
+    export PEER_CONTAINER_NAME=peer0.org2.network.com
+
+    docker exec ${PEER_CONTAINER_NAME} peer lifecycle chaincode install ${CC_NAME}.tar.gz
+
+    # do something ...
+}
+
+install-chaincode-peer0-org1
+install-chaincode-peer0-org2
 
 echo "====== approve Chaincode ======"
 
