8c2e431f9f395c611fed48420ab11012e3f24219
MASDXI <sirawitt42@gmail.com>
2020-12-08 11:55:20 +0700

    create shell script code structure for deployCC

---
+++ frist-network/deployCC.sh
@@ -0,0 +1,31 @@
+# env
+export PEER0_ORG1=peer0.org1.network.com
+export PEER0_ORG2=peer0.org2.network.com
+# export =
+# more...
+
+# chaincode env
+export CC_VERSION=1.0
+export CC_NAME=fabcar
+# export =
+# more...
+
+echo "============ Deploy Chaincode ============"
+
+# do something ...
+
+echo "====== Package Chaincode ======"
+
+# do something ...
+
+echo "====== Install Chaincode ======"
+
+# do something ...
+
+echo "====== query Installed Chaincode ======"
+
+# do something ...
+
+echo "====== approve Chaincode ======"
+
+# do something ...
